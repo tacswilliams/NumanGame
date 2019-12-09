f4cf597e58d6ff0afcafcfb6d73545598e2d7050
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2019-12-09 18:09:49 -0500

    Wrok done on12.9.2019

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour { 
+
+    public Rigidbody rb;
+    public Playercolleshion pc;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pc = GetComponent<Playercolleshion>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() 
+    {
+        if (pc.groundHit) {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            }
+
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Playercolleshion.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Playercolleshion : MonoBehaviour
+{
+    public PlayerMovement movement;
+    public bool groundHit = false;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+
+            //movement.enabled = false;
+
+        }
+        if (collisionInfo.collider.tag == "Ground")
+        {
+            groundHit = true;
+        }
+
+
+
+    }
+    
+  } 
+
