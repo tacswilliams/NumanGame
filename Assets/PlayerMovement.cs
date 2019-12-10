3047d42fa0f592bbe93147b67f5db10ea39f0b95
tacswilliams <55443380+tacswilliams@users.noreply.github.com>
2019-12-10 15:01:40 -0500

    Repositions player after collision

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour {
     public Playercolleshion pc;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
-
+    
+    public float timer = 0f;
     // Start is called before the first frame update
 
     private void Start()
@@ -15,9 +16,39 @@ public class PlayerMovement : MonoBehaviour {
         pc = GetComponent<Playercolleshion>();
     }
 
+    public void ResetPlayer()
+    {
+        //roatation
+        
+
+        //postition
+
+
+        //ground hit false
+
+    }
+    
+        
+    
+
     // Update is called once per frame
     void FixedUpdate() 
     {
+        // repositions player to face front bsed on a timer
+        if (pc.obstacleHit && timer < 1)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, timer);
+            timer = timer + Time.deltaTime;
+
+        }
+
+        // Resets the timer and obstacle hit variable
+        if(timer > 1)
+        {
+            timer = 0f;
+            pc.obstacleHit = false;
+        }
+
         if (pc.groundHit) {
             rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
--- Assets/Playercolleshion.cs
+++ Assets/Playercolleshion.cs
@@ -5,11 +5,12 @@ public class Playercolleshion : MonoBehaviour
 {
     public PlayerMovement movement;
     public bool groundHit = false;
+    public bool obstacleHit = false;
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
-
+            obstacleHit = true;
             //movement.enabled = false;
 
         }
